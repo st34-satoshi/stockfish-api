2477cbadae1d12158e417a417e3319078c5dda36
Satoshi Tanaka <st34.satoshi@gmail.com>
2023-10-16 23:47:00 +0900

    calculate best move

--- app/main.py
+++ app/main.py
@@ -1,16 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from models.engine import ChessEngine
 import logging
 
 logging.basicConfig(filename='log/logger.log', encoding='utf-8', level=logging.INFO)
 
 app = FastAPI()
 
+CHESS_ENGINE = ChessEngine()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/analysis")
+async def callback(request: Request, position: str):
+    logging.info(f'position = {position}')
+    logging.info(f'request = {request}')
+    CHESS_ENGINE.start_engin()
+    # TODO: position is correct?
+    res = CHESS_ENGINE.best_move(position)
+    # res = CHESS_ENGINE.best_move('rnbqkbnr/pppppppp/8/8/6P1/8/PPPPPP1P/RNBQKBNR b KQkq - 0 1')
+    return {"message": "ok", "position": position, "score": res["score"], "mate": res["mate"], "best_move": res["best_move"]}
---
+++ app/models/engine.py
@@ -0,0 +1,54 @@
+import subprocess
+
+class ChessEngine:
+    """
+    communicate with stockfish
+    """
+
+    def __init__(self) -> None:
+        self.p = subprocess.Popen(
+            ("Stockfish/src/stockfish"),
+            stdout=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+        )
+
+    def kill(self) -> None:
+        self.p.kill()
+
+    def start_engin(self) -> None:
+        self.request("uci", "uciok")
+        self.request("isready", "readyok")
+
+    def request(self, input_text, expect_response):
+        """
+        input text and wait until getting expect response
+        """
+        self.p.stdin.write(f"{input_text}\n".encode())
+        self.p.stdin.flush()
+        output = ""
+        while expect_response != output:
+            # output = self.p.stdout.readline
+            output = self.p.stdout.readline().decode().strip()
+
+    def best_move(self, fen_position):
+        """
+        return {mate: 2, score: 100, best_move: e2e4}
+        if lose, mate is minus
+        """
+        self.p.stdin.write(f"position fen {fen_position}\n".encode())
+        self.p.stdin.write("go depth 25 \n".encode())
+        self.p.stdin.flush()
+        output = ""
+        res = {"mate": None, "score": None, "best_move": None}
+        while "bestmove" not in output:
+            output = self.p.stdout.readline().decode().strip()
+            if "score cp" in output:
+                score = output.split('cp ')[1].split(' ')[0]
+                res["score"] = score
+            if "score mate" in output:
+                mate = output.split('mate ')[1].split(' ')[0]
+                res["mate"] = mate
+            if "bestmove" in output:
+                best_move = output.split('bestmove ')[1].split(' ')[0]
+                res["best_move"] = best_move
+        return res
